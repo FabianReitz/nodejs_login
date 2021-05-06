0f8e8f01274a1124ef3d474af7e920e257d2c7ed
FabianReitz <reitz@stadtwerk.org>
2021-05-07 00:09:37 +0200

    [📄] Added new script to populate mongoDB with custom users

---
+++ populateDatabase.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import Model from './models/user.js';
+
+const CONNECTION_URL = `mongodb://localhost:27017/database`;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error: '));
+
+db.once('open', function () {
+    console.log('Successfully connected to database!');
+});
+
+const testUser = new Model({ username: 'testUser', password: '1234' });
+
+testUser.save(function(err, doc) {
+    if (err) return console.error(err);
+    console.log('User has been created!');
+    process.exit();
+});
